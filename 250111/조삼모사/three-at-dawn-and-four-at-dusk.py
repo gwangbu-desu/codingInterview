12074181ffe38d3b28391e4e46752a1d41db3159
광부입니다 <kwy1379@ajou.ac.kr>
2025-01-11 00:47:17 +0900

    Title: 조삼모사, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250111/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+exit_condition = n//2
+
+minimum = 100000
+
+def cal(lst):
+    result = 0
+    for i in range(exit_condition):
+        for j in range(i):
+            result += arr[lst[i]][lst[j]] + arr[lst[j]][lst[i]]
+    return result
+
+def back(alst, blst, idx):
+    global minimum
+    if len(alst) == len(blst) and idx == n:
+        minimum = min(minimum, abs(cal(alst) - cal(blst)))
+        return
+    if len(alst) >3 or len(blst)>3:
+        return
+
+    back(alst+[idx], blst, idx+1)
+    back(alst,blst + [idx], idx+1)
+
+back([],[],0)
+print(minimum)

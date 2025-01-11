6d8dcc0114f3726380bbcb0532d9551d92f41c09
광부입니다 <kwy1379@ajou.ac.kr>
2025-01-11 22:31:59 +0900

    Title: 바이러스 검사, Time: 278ms, Memory: 108MB, Status: Passed - CodeTree

--- 250111/바이러스 검사/virus-detector.py
+++ 250111/바이러스 검사/virus-detector.py
@@ -7,9 +7,11 @@ total = 0
 
 for i in custom_n:
     i -= boss
-    total += 1 + i // member
-    i = i%member
+    total += 1
     if i > 0:
-        total += 1
+        total += i // member
+        i = i%member
+        if i > 0:
+            total += 1
 
 print(total)

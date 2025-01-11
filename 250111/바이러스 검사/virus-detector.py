26d11ca3c6a50c979b99b9e67a9eff09e8441c87
광부입니다 <kwy1379@ajou.ac.kr>
2025-01-11 22:26:32 +0900

    Title: 바이러스 검사, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250111/바이러스 검사/virus-detector.py
@@ -0,0 +1,12 @@
+n = int(input())
+custom_n = list(map(int,input().split()))
+
+boss, member = map(int,input().split())
+
+total = 0
+
+for i in custom_n:
+    i -= boss
+    total += 1 + i // member + i % member
+
+print(total)

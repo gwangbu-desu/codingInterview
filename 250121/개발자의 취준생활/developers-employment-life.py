ec5408af92b4d2a5f41f017b6bd03ac25873490d
광부입니다 <kwy1379@ajou.ac.kr>
2025-01-21 21:51:26 +0900

    Title: 개발자의 취준생활, Time: 5091ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 250121/개발자의 취준생활/developers-employment-life.py
+++ 250121/개발자의 취준생활/developers-employment-life.py
@@ -1,17 +1,23 @@
 n = int(input())
-
-employee = list(map(int,input().split()))
-
-company = list(map(int,input().split()))
+employee = list(map(int, input().split()))
+company = list(map(int, input().split()))
 
 ans = 0
-def bt(remains:list,idx:int):
-    global ans
-    if idx == n-1 and remains[0] >= employee[idx]:
-        ans += 1
-    for index,value in enumerate(remains):
-        if value >= employee[idx]:
-            bt(remains[:index] + remains[index+1:], idx+1)
 
-bt(company,0)
+def bt(used: list, idx: int):
+    global ans
+    if idx == n:  # 모든 직원의 요구를 충족한 경우
+        ans += 1
+        return
+
+    for i, c in enumerate(company):
+        if not used[i] and c >= employee[idx]:
+            used[i] = True  # 회사 자원을 사용
+            bt(used, idx + 1)
+            used[i] = False  # 되돌리기
+
+# 회사 자원 사용 여부를 추적하기 위한 배열
+used = [False] * n
+bt(used, 0)
+
 print(ans)

1297eb95d034aedf5e477c3a30b712d692cbd601
광부입니다 <kwy1379@ajou.ac.kr>
2025-01-21 22:38:17 +0900

    Title: 개발자의 취준생활, Time: 574ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 250121/개발자의 취준생활/developers-employment-life.py
+++ 250121/개발자의 취준생활/developers-employment-life.py
@@ -1,24 +1,23 @@
-from itertools import permutations
 
-# 입력
 n = int(input())
 employee = list(map(int, input().split()))
 company = list(map(int, input().split()))
 
-# 가능한 회사 순열 생성
-company_indices = list(range(n))
-permutations_of_companies = permutations(company_indices)
+# DP 테이블 초기화
+# dp[i][used]는 i번째 직원까지 배치했을 때, 사용된 회사 상태가 used일 경우 가능한 경우의 수
+dp = [[0] * (1 << n) for _ in range(n + 1)]
+dp[0][0] = 1  # 초기 상태
 
-# 가능한 배치 수 계산
-result = 0
-for perm in permutations_of_companies:
-    valid = True
-    for i in range(n):
-        if company[perm[i]] < employee[i]:
-            valid = False
-            break
-    if valid:
-        result += 1
+# 회사 사용 상태를 리스트로 대체
+used_states = [[False] * n for _ in range(1 << n)]
 
-# 출력
-print(result)
+for i in range(n):  # i번째 직원
+    for mask in range(1 << n):  # 모든 사용 상태 탐색
+        if dp[i][mask] == 0:  # 유효하지 않은 상태는 건너뜀
+            continue
+
+        for j in range(n):  # j번째 회사
+            if not (mask & (1 << j)) and company[j] >= employee[i]:
+                dp[i + 1][mask | (1 << j)] += dp[i][mask]  
+
+print(dp[n][(1 << n) - 1])

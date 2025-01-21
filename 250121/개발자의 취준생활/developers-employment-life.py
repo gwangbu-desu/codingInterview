8dc74907fbc5099347b30a3637b0c293302f326c
광부입니다 <kwy1379@ajou.ac.kr>
2025-01-21 20:56:48 +0900

    Title: 개발자의 취준생활, Time: 100ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 250121/개발자의 취준생활/developers-employment-life.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+employee = list(map(int,input().split()))
+
+company = list(map(int,input().split()))
+
+ans = 0
+def bt(remains:list,idx:int):
+    global ans
+    if idx == 3 and remains[0] >= employee[idx]:
+        ans += 1
+    for index,value in enumerate(remains):
+        if value >= employee[idx]:
+            bt(remains[:index] + remains[index+1:], idx+1)
+
+bt(company,0)
+print(ans)

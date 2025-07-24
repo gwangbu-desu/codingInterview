ecaa6f1e693e5bc9df352d885fbdcab98b9bfe9a
광부입니다 <kwy1379@ajou.ac.kr>
2025-07-25 02:29:23 +0900

    [Gold I] Title: 낚시왕, Time: 4020 ms, Memory: 208952 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -2,17 +2,16 @@ import sys
 input = sys.stdin.readline
 
 R, C, M = map(int,input().split()) # 격자판 r,c 상어수 M
-arr = [[False] * (C+1) for _ in range(R+1)] # R x C
+arr = [[None] * (C+1) for _ in range(R+1)] # R x C
 
 dc = [0,0,1,-1]
 dr = [-1,1,0,0]
 
-shark_list = dict()
+# shark_list = dict()
 answer = 0
 for _ in range(M):
     r,c,s,d,z = map(int,input().split()) # 위치 r,c 속력 s 이동 방향 d 크기 z
-    shark_list[(r-1,c-1)] = (s,d-1,z)
-    arr[r-1][c-1] = True
+    arr[r-1][c-1] = (s,d-1,z)
 ###################### 입력 끝
 def change_dir(d):
     if d == 0:
@@ -47,7 +46,7 @@ def check(nr,nc):
         return True
     return False
 
-for i in range(0,C): # 1th-index, 1. 낚시왕 이동
+for i in range(C): # 1th-index, 1. 낚시왕 이동
     # print("상어 이동 전:")
     # for a in range(R):
     #     print(*arr[a], sep=' ')
@@ -56,27 +55,24 @@ for i in range(0,C): # 1th-index, 1. 낚시왕 이동
     # 2. 상어 잡기.
     for j in range(R):
         if arr[j][i]:
-            s,d,z = shark_list.pop((j,i)) # 상어 잡기
+            s,d,z = arr[j][i] # 상어 잡기
             answer += z
-            arr[j][i] = False # 상어 삭제
+            arr[j][i] = None # 상어 삭제
             break # 제일 위 잡으면 종료
     # 3. 상어 이동.
-    new_shark_list = dict()
-    for key, value in shark_list.items():
-        r,c = key
-        s,d,z = value
-        nr, nc, nd = shark_move(r,c,s,d) # 이동 완료
-        if new_shark_list.get((nr,nc),None): # 있다면 크기 비교
-            _,_,origin = new_shark_list.get((nr,nc))
-            if origin < z:
-                new_shark_list[(nr,nc)] = (s,nd,z)
-        else: # 해당 칸에 상어가 없으면 저장
-            new_shark_list[(nr,nc)] = (s,nd,z)
-        arr[r][c] = False # 기존 상어 위치는 없애기
-    shark_list = new_shark_list # 새 상어 위치로 덮어버림
-    for key, value in shark_list.items():
-        r,c = key
-        arr[r][c] = True # 새 상어 위치
+    temp = [[None] * (C+1) for _ in range(R+1)]
+    for r in range(R):
+        for c in range(C):
+            if arr[r][c]:
+                s, d, z = arr[r][c]
+                nr, nc, nd = shark_move(r, c, s, d)
+                if temp[nr][nc]:
+                    # 큰 상어만 살아남음
+                    if temp[nr][nc][2] < z:
+                        temp[nr][nc] = (s, nd, z)
+                else:
+                    temp[nr][nc] = (s, nd, z)
+    arr = temp  # 다음 턴 상태로 갱신
     # for a in range(R):
     #     print(*arr[a], sep=' ')
     #     print()

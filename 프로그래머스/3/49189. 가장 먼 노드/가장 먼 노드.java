709632eb22bb994f62738582b4dae1b6b26b3487
광부입니다 <kwy1379@ajou.ac.kr>
2025-11-11 12:40:43 +0900

    [level 3] Title: 가장 먼 노드, Time: 224.92 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,58 +1,41 @@
 import java.util.*;
-
 class Solution {
-    ArrayList<ArrayList<Integer>> adj_graph = new ArrayList<>();
-    boolean[] visited = new boolean[20001];
-    int[] distance = new int[20001];
-    public void start(){
-        int start = 1;
-        int start_length = 0;
-        Integer node;
-        Integer next_node;
-        ArrayList<Integer> graph;
-        Arrays.fill(distance,50000);
-        distance[1] = 0;
-        ArrayDeque<Integer> dq = new ArrayDeque<>(); //길이, 노드
-        dq.add(1);
-        while(!dq.isEmpty()){
-            node = dq.poll();
-            graph = adj_graph.get(node);
-            for(int i=0;i<graph.size();i++){
-                next_node = graph.get(i); // 다음 방문 노드
-                if(distance[next_node] > distance[node] + 1){
-                    distance[next_node] = distance[node] + 1;                
-                    dq.add(next_node);
-                }
-            }
-        }
-    }
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        for(int i=0;i<=n;i++){
-            adj_graph.add(new ArrayList<>());
-        }
-        for(int[] each_edge:edge){
-            adj_graph.get(each_edge[1]).add(each_edge[0]);
-            adj_graph.get(each_edge[0]).add(each_edge[1]);
-        }
         
-        start();
-        int maximum=0;
-        int count=0;
-        for(int i=0;i<20001;i++){
-            if(distance[i] == 50000){
-                continue;
-            }
-            
-            // System.out.print(distance[i]);
-            if(distance[i] > maximum){
-                maximum = distance[i];
-                answer = 1;
-            }else if(distance[i] == maximum){
-                answer+=1;
+        int[] distance  = new int[n+1]; // 1-th index
+        boolean[] visited = new boolean[n+1];
+        ArrayList<Integer>[] edges = new ArrayList[n+1];
+        int answer = 0;
+        
+        for(int i = 0;i<=n;i++){ // 간선 담을 배열
+            edges[i] = new ArrayList<>();
+        }
+        for(int[] _edge : edge){
+            edges[_edge[0]].add(_edge[1]);
+            edges[_edge[1]].add(_edge[0]);
+        }
+        Queue<Integer> dq = new ArrayDeque<>();
+        dq.add(1);
+        visited[1] = true;
+        while(!dq.isEmpty()){
+            int temp = dq.poll();
+            for(Integer dist: edges[temp]){ // 
+                if(visited[dist]) continue; // 방문한곳 = 최단 거리가 구해진곳은 건너뛰기.
+                visited[dist] = true;
+                dq.add(dist);
+                distance[dist] = distance[temp] + 1;
+                
             }
         }
-        // System.out.println(count);
+        Arrays.sort(distance);
+        int max = distance[n];
+        // System.out.println(max);
+        for(int i: distance){
+            System.out.println(i);
+            if ( i == max){
+                answer ++;
+            }
+        }
         return answer;
     }
 }

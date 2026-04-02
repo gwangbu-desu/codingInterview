8134e9a2b110791b5112c5379aa90bb9c825c6be
광부입니다 <kwy1379@ajou.ac.kr>
2026-04-02 23:00:00 +0900

    [level 3] Title: 베스트앨범, Time: 18.03 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,35 +1,61 @@
 import java.util.*;
 
 class Solution {
+    HashMap<String,Integer> genreCount;
+    class SongInfo implements Comparable<SongInfo>{
+        String Genre;
+        int SongCount;
+        int idx;
+        
+        public SongInfo(String gc, int sc, int idx){
+            this.Genre = gc;
+            this.SongCount = sc;
+            this.idx = idx;
+        }
+        
+        @Override
+        public int compareTo(SongInfo other) {
+            if (!genreCount.get(this.Genre).equals(genreCount.get(other.Genre))) {
+                return genreCount.get(other.Genre) - genreCount.get(this.Genre);
+            }
+            if (this.SongCount != other.SongCount) {
+                return other.SongCount - this.SongCount;
+            }
+            return this.idx - other.idx;
+        }
+        @Override
+        public String toString(){
+            return this.Genre + this.SongCount + this.idx;
+        }
+    }
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        HashMap<String,Integer> genreMap = new HashMap<>();
-        PriorityQueue<int[]> ranking = new PriorityQueue<>((a,b) ->{
-            if (b[0] != a[0]){            
-                return b[0] - a[0];   
+        List<Integer> answer = new ArrayList<>();
+        ArrayList<SongInfo> songList = new ArrayList<>();
+        genreCount = new HashMap<>();
+        for(int i=0; i<plays.length;i++){
+            genreCount.put(genres[i], genreCount.getOrDefault(genres[i], 0) + plays[i]);
+            songList.add(new SongInfo(genres[i], plays[i], i));
+        }
+        Collections.sort(songList);
+        
+        int beforeCount = 0;
+        int cnt = 0;
+        for(SongInfo i : songList){
+            if (beforeCount == genreCount.get(i.Genre)){
+                cnt ++;
             }else{
-                if(a[1] != b[1]){
-                    return b[1] - a[1];
-                }
-                return a[2]-b[2];
+                cnt = 0;
+                beforeCount = genreCount.get(i.Genre);
             }
-        });
-        for(int i=0; i<genres.length;i++){
-            genreMap.put(genres[i], genreMap.getOrDefault(genres[i],0) + plays[i]);
-        }
-        // 장르 랭킹 결정
-        for(int i=0;i<plays.length;i++){
-            ranking.add(new int[]{genreMap.get(genres[i]),plays[i],i});
-        }
-        List<Integer> tmp = new ArrayList<>();
-        while(!ranking.isEmpty()){
-            int genreTotal = ranking.peek()[0];
-            for(int i=0;i<2;i++){
-                if(ranking.isEmpty() || ranking.peek()[0] != genreTotal) break;
-                tmp.add(ranking.poll()[2]);
+            if (cnt >= 2){
+                continue;
             }
-            while(!ranking.isEmpty() && ranking.peek()[0] == genreTotal) ranking.poll();
+            // System.out.println(cnt);
+            answer.add(i.idx);
         }
-        return tmp.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
+
+// 장르별로 2개씩.
+// 정렬 기준 : 앨범 수록 기준 장르 count 높을 수록, 재생 count 높을 수록, 고유번호는 오름차순
